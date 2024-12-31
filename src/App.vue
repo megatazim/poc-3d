92564e8c23276f8b5935d4244c9194068b11b47e
严一笑 <xiaoyanzero@126.com>
2024-12-31 14:47:57 +0800

    Init SuperMap3D 20241231

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,7 @@
 <script setup>
-import { onMounted } from "vue";
-import { settings } from "@/setting";
-
-// 定义组件
-const initViewer = () => {
-  if (window.viewer) {
-    window.viewer.destroy();
-    window.viewer = null;
-  }
-  const viewer = new SuperMap3D.Viewer("SuperMap3DContainer", {
-    // navigation: false,
-    contextOptions: {
-      contextType: Number(settings.SuperMap3DOptions.EngineType), // Webgl2:2 ; WebGPU:3
-    },
-  });
-  window.viewer = viewer;
-  console.log("1111");
-};
-
-// 组件挂载时执行 initViewer 函数
-onMounted(() => {
-  try {
-    initViewer();
-  } catch (error) {
-    console.log(error);
-  }
-});
+import SuperMap3D from "@/components/SuperMap3D.vue";
 </script>
 
 <template>
-  <div id="SuperMap3DContainer"></div>
+  <SuperMap3D></SuperMap3D>
 </template>
-
-<style scoped>
-#SuperMap3DContainer {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  margin: 0;
-  overflow: hidden;
-  padding: 0;
-  font-family: sans-serif;
-}
-</style>
---
+++ src/components/SuperMap3D.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { onMounted } from "vue";
+import { settings } from "@/setting";
+
+// 定义组件
+const initViewer = () => {
+  if (window.viewer) {
+    window.viewer.destroy();
+    window.viewer = null;
+  }
+  const viewer = new SuperMap3D.Viewer("SuperMap3DContainer", {
+    // navigation: false,
+    contextOptions: {
+      contextType: Number(settings.SuperMap3DOptions.EngineType), // Webgl2:2 ; WebGPU:3
+    },
+  });
+  window.viewer = viewer;
+};
+
+// 组件挂载时执行 initViewer 函数
+onMounted(() => {
+  try {
+    initViewer();
+  } catch (error) {
+    console.log(error);
+  }
+});
+</script>
+
+<template>
+  <div id="SuperMap3DContainer"></div>
+</template>
+
+<style scoped>
+#SuperMap3DContainer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  overflow: hidden;
+  padding: 0;
+  font-family: sans-serif;
+}
+</style>
